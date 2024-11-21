bf2db14d7b2cc1b4640188b00e62a34e9e82064b
Fauziah Permatasari <fauziah.permatasari@sap.com>
2024-11-21 10:11:33 +0100

    Return input text and input validation

--- 01-Build-A-Palindrome/script.js
+++ 01-Build-A-Palindrome/script.js
@@ -6,3 +6,27 @@ function isPalindrome(str) {
 	const cleanStr = str.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
 	return cleanStr === [...cleanStr].reverse().join('');
 }
+
+function displayResult(input, isPalindrome) {   // Function to display user input after going through isPalindrome()
+	resultDiv.textContent = ''; // Clear previous result
+
+	const message = document.createElement('p');
+	message.className = 'user-input';
+	message.textContent = `"${input}" ${isPalindrome ? 'is' : 'is not'} a palindrome.`;
+	resultDiv.appendChild(message);
+
+	resultDiv.classList.remove('hidden');
+}
+
+function handleCheckPalindrome() {  // Input validation
+	const inputValue = userInput.value.trim();
+
+	if (inputValue === '') {
+		alert('Please input a value');
+		return;
+	}
+
+	displayResult(inputValue, isPalindrome(inputValue));
+	userInput.value = ''; // Clear input field
+}
+
